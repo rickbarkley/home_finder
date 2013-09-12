bd8cbcf53b0cd9b44f4c2e14c9a34012fecd63c3
rick_barkley <rick_barkley@nuvola-networks.com>
2013-09-11 23:09:56 -0500

    updated create and edit path

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,4 @@
 class Plan < ActiveRecord::Base
-  attr_accessible :bathrooms, :bedrooms, :levels, :price_range, :sqfoot, :drawing
+  attr_accessible :name, :bathrooms, :bedrooms, :levels, :price_range, :sqfoot, :drawing
   has_attached_file :drawing,  :default_url => "/images/:style/missing.pdf"
   end
